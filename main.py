0a18e7ff83ae9fd405e7d69ebfc3476eb4681d63
khakimovazizbek21o2-source <khakimovazizbek21o2@gmail.com>
2026-08-11 03:04:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import os
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from handlers import router
 
-# Telegram bot tokeningizni o'zgaruvchiga qo'ying
-BOT_TOKEN = "8668763966:AAGOSeYgbeWGwsv-nRFtfONwpPCSMvIAwLM"
+# Tokenni Render Environment Variable'dan oladi, topsa ishlatadi, bo'lmasa o'zidagini
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8668763966:AAGOSeYgbeWGwsv-nRFtfONwpPCSMvIAwLM")
 
 async def main():
     logging.basicConfig(level=logging.INFO)
@@ -15,9 +16,9 @@ async def main():
     # Router'ni ulash
     dp.include_router(router)
     
-    # Webhook bo'lsa tozalab, pollingni boshlash
+    # Webhook'ni tozalash va polling boshlash
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(main())  # <-- Bu yerda 4 ta probel surilgan bo'lishi shart!
