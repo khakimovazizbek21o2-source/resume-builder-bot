408b48c6980b560453792e3b7e1d9207d30bc250
khakimovazizbek21o2-source <khakimovazizbek21o2@gmail.com>
2026-08-11 03:00:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
-from config import BOT_TOKEN
 from handlers import router
+
+# Telegram bot tokeningizni o'zgaruvchiga qo'ying
+BOT_TOKEN = "8668763966:AAGOSeYgbeWGwsv-nRFtfONwpPCSMvIAwLM"
 
 async def main():
     logging.basicConfig(level=logging.INFO)
+    
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
     
+    # Router'ni ulash
     dp.include_router(router)
     
+    # Webhook bo'lsa tozalab, pollingni boshlash
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
